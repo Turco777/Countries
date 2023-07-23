b22ba01512f98476cea3b1fd7e5241faba7ba87c
Turco777 <agusalmona@gmail.com>
2023-07-23 18:47:01 -0300

    Backend last commit

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,4 +2,14 @@ const { Router } = require("express");
 
 const router = Router();
 
+const getAllCountries = require("../controllers/getAllCountries.js");
+const getCountryById = require("../controllers/getCountryById.js");
+const postActivities = require("../controllers/postActivities.js");
+const getActivities = require("../controllers/getActivities.js");
+
+router.get("/countries", getAllCountries);
+router.get("/countries/:idPais", getCountryById);
+router.post("/activities", postActivities);
+router.get("/activities", getActivities);
+
 module.exports = router;
